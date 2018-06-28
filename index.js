b8c0afb247efaa9aaeb7a3a29a5cd2ec8acdc437
Joshua Leiner <jleiner96@gmail.com>
2018-06-28 17:10:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+
+function whisper(string) {
+  return string.toLowerCase ()
+}
+
+    
+function logshout (strig) {
+  console.log(logshout.toUpperCase)
+}
+
